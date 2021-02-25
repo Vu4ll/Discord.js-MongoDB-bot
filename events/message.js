8e70824c7cdd6ca3c6d90ff4691970de41e768ed
Vu4ll <63701059+Vu4ll@users.noreply.github.com>
2021-02-25 19:48:03 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const ayarlar = require('../ayarlar.json');
+const config = require('../config/config.json');
 let talkedRecently = new Set();
 module.exports = message => {
   if (talkedRecently.has(message.author.id)) {
@@ -10,10 +10,9 @@ module.exports = message => {
   }, 2500);
   let client = message.client;
   if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  if (!message.content.startsWith(config.prefix)) return;
+  let command = message.content.split(' ')[0].slice(config.prefix.length);
   let params = message.content.split(' ').slice(1);
-  let perms = client.elevation(message);
   let cmd;
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
@@ -21,8 +20,7 @@ module.exports = message => {
     cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
+    cmd.run(client, message, params);
   }
 
 };
